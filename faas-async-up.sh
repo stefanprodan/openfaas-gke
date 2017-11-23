74a910f81dc807efa54febfcdd024a1330897036
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-23 20:35:49 +0200

    create external LB from yml

--- faas-async-up.sh
+++ faas-async-up.sh
@@ -8,18 +8,18 @@ get_ingress_ip() {
     kubectl -n openfaas describe service gateway-lb | grep Ingress | awk '{ print $NF }'
 }
 
-kubectl apply -f ./ns.yml,faas-async.yml,nats.yml,prom.yml
+kubectl apply -f ./ns.yml,gateway-lb.yml,nats.yml,faas-async.yml,prom.yml
 
-kubectl -n openfaas expose deployment gateway \
-    --type=LoadBalancer \
-    --name=gateway-lb
-
-kubectl -n openfaas get all
+#kubectl -n openfaas expose deployment gateway \
+#    --type=LoadBalancer \
+#    --name=gateway-lb
 
 until [[ "$(get_ingress_ip)" ]]
  do sleep 1;
  echo -n ".";
 done
+
+kubectl -n openfaas get all
 
 echo ""
 echo "External IP: $(get_ingress_ip)"
