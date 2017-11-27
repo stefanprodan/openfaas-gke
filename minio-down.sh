dcbbb7d5b77597cd47e82de077ca2d6f27d6f522
Stefan Prodan <stefan.prodan@gmail.com>
2017-11-27 17:13:11 +0200

    cleanup

--- minio-down.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -o errexit
-set -o pipefail
-set -o nounset
-
-kubectl delete -f ./minio.yml
-kubectl -n openfaas-fn delete service minio-lb
-kubectl -n openfaas-fn delete secret minio-auth
